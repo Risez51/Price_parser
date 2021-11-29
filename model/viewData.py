913840023d73016a739e949452c6fa7de2493e8a
Risez51 <squexke@gmail.com>
2021-11-29 15:00:28 +0300

    Готовая GUI

---
+++ model/viewData.py
@@ -0,0 +1,4 @@
+class ViewData():
+    def __init__(self):
+
+        pass
--- view/mainForm.py
+++ view/mainForm.py
@@ -1,4 +1,5 @@
 import wx
+from wx.lib.agw import ultimatelistctrl as ULC
 class MainForm(wx.Frame):
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, title=title, size=(600, 400))
@@ -27,12 +28,14 @@ class MainForm(wx.Frame):
         #Первая строка (добавление файла: отчет/выгрузка(china2) из CheesCake)
         hbox1 = wx.BoxSizer(wx.HORIZONTAL)
         label_CheesCake = wx.StaticText(self.mainPanel, label="Отчет чизкейк:")
-        input_CheesCake = wx.TextCtrl(self.mainPanel, style=wx.TE_READONLY)
+        self.input_CheesCake = wx.TextCtrl(self.mainPanel, style=wx.TE_READONLY)
         buttonOpenCheesCakeFile = wx.Button(self.mainPanel, wx.ID_ANY, "Добавить")
         hbox1.Add(label_CheesCake, flag=wx.RIGHT, border=8)
-        hbox1.Add(input_CheesCake, proportion=1)
+        hbox1.Add(self.input_CheesCake, proportion=1)
         hbox1.Add(buttonOpenCheesCakeFile, flag=wx.RIGHT | wx.LEFT, border=8)
 
+        #бинды первой строки Cheescake:
+        self.Bind(wx.EVT_BUTTON, self.add_Cheescake_Report, buttonOpenCheesCakeFile)
 
         #линия сепаратор 15
         hbox15 = wx.BoxSizer(wx.HORIZONTAL)
@@ -42,50 +45,81 @@ class MainForm(wx.Frame):
 
         #Вторая строка (добавление файла соответствий)
         hbox2 = wx.BoxSizer(wx.HORIZONTAL)
-        label_comparision = wx.StaticText(self.mainPanel, label="Таблица \nсоответствий:")
-        input_comparision = wx.TextCtrl(self.mainPanel, style=wx.TE_READONLY)
+        label_comparision = wx.StaticText(self.mainPanel, label="Таблица            \nсоответствий:")
+        self.input_comparision = wx.TextCtrl(self.mainPanel, style=wx.TE_READONLY)
         buttonOpenComparisionFile = wx.Button(self.mainPanel, wx.ID_ANY, "Добавить")
         hbox2.Add(label_comparision, flag=wx.RIGHT, border=8)
-        hbox2.Add(input_comparision, proportion=1)
+        hbox2.Add(self.input_comparision, proportion=1)
         hbox2.Add(buttonOpenComparisionFile, flag=wx.RIGHT | wx.LEFT, border=8)
 
+        # бинды второй строки Таблица-соответствий:
+        self.Bind(wx.EVT_BUTTON, self.add_Comparision_Report, buttonOpenComparisionFile)
 
         # Добавление прайсов на лист
         hbox25 = wx.BoxSizer(wx.HORIZONTAL)
-        buttonOpenSupplierPrices = wx.Button(self.mainPanel, wx.ID_ANY, label="Добавить прайсы поставщиков", size=(0,40))
+        buttonOpenSupplierPrices = wx.Button(self.mainPanel, wx.ID_ANY, label="Добавить прайсы поставщиков", size=(0,
+                                                                                                                   40))
         buttonOpenSupplierPrices.Bind(wx.EVT_BUTTON, self.openSupplierFiles)
         hbox25.Add(buttonOpenSupplierPrices, flag=wx.EXPAND, proportion=1)
 
 
         #Рабочая область - таблица добавленных файлов поставщика:
-        self.hbox3 = wx.BoxSizer(wx.VERTICAL)
-        self.workPanel = wx.Panel(self.mainPanel,size=(600,300))
-        self.workPanel.SetBackgroundColour(wx.GREEN)
-        self.hbox3.Add(self.workPanel)
+        self.l = ["Дарси", "Мир инструмента"]
+        self.hbox3 = wx.BoxSizer(wx.HORIZONTAL)
+        self.ulc = ULC.UltimateListCtrl(self.mainPanel, agwStyle=ULC.ULC_HAS_VARIABLE_ROW_HEIGHT | ULC.ULC_REPORT)
+        self.ulc.InsertColumn(0, "Файл", width=350)
+        self.ulc.InsertColumn(1, "Поставщик", width=215)
+        self.hbox3.Add(self.ulc, proportion=1, flag=wx.EXPAND)
+
 
         self.l = ["Дарси", "Мир инструмента"]
+
         #Кнопка управление приложением Спарсить/Очистить
         hbox4 = wx.BoxSizer(wx.HORIZONTAL)
-        buttonParse = wx.Button(self.mainPanel, wx.ID_ANY, label="Спарсить", size=(70,30))
-        buttonDeleteFile = wx.Button(self.mainPanel, wx.ID_ANY, label="Очистить", size=(70,30))
-        hbox4.Add(buttonParse, flag=wx.LEFT, border=10)
-        hbox4.Add(buttonDeleteFile, flag=wx.LEFT, border=10)
+
+        buttonParse = wx.Button(self.mainPanel, wx.ID_ANY, label="Спарсить", size=(90, 30))
+        buttonParse.SetBackgroundColour(wx.GREEN)
+        buttonClearAllUlc = wx.Button(self.mainPanel, wx.ID_ANY, label="Очистить все", size=(90, 30))
+        buttonDeleteRow = wx.Button(self.mainPanel, wx.ID_ANY, label="Удалить файл", size=(90, 30))
+
+        hbox4.Add(buttonDeleteRow, flag=wx.ALIGN_LEFT)
+        hbox4.Add(buttonClearAllUlc, flag=wx.LEFT | wx.RIGHT, border=10)
+        hbox4.Add(buttonParse, flag=wx.LEFT | wx.RIGHT, border=10)
+
+
+        #бинды кнопок управления
+        self.Bind(wx.EVT_BUTTON, self.deleteRowInUlc, buttonDeleteRow)
+        self.Bind(wx.EVT_BUTTON, self.getDataFromForm, buttonParse)
+        self.Bind(wx.EVT_BUTTON, self.clearULC, buttonClearAllUlc)
 
 
         #добавление элементов в главный сайзер приложения VBOX
         self.vbox.Add(hbox1, flag=wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, border=10)
         self.vbox.Add(hbox2, flag=wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, border=10)
-        self.vbox.Add(hbox15, flag=wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, border=5)
-        self.vbox.Add(hbox25, flag=wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, border=5)
-        self.vbox.Add(self.hbox3, proportion=1, flag=wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, border=10)
+        self.vbox.Add(hbox15, flag=wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, border=10)
+        self.vbox.Add(hbox25, flag=wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, border=10)
+        self.vbox.Add(self.hbox3, proportion=1, flag=wx.EXPAND | wx.BOTTOM | wx.LEFT | wx.RIGHT | wx.TOP, border=10)
         self.vbox.Add(hbox4, flag=wx.ALIGN_RIGHT | wx.BOTTOM | wx.RIGHT | wx.TOP, border=10)
 
-
+        self.vbox.Fit(self.mainPanel)
         #Добавление VBOX на главную панель рабочей области
         self.mainPanel.SetSizer(self.vbox)
 
 
+    def getDataFromForm(self, event):
+        rowsCount = self.ulc.GetItemCount()
+        for i in range(0, rowsCount):
+            a = self.ulc.GetItemText(i)
+            b = self.ulc.GetItemWindow(i, 1)
+            ind = b.GetSelection()
+            print(a, b.GetString(ind))
 
+    def clearULC(self, event):
+        self.ulc.DeleteAllItems()
+
+    def deleteRowInUlc(self, event):
+       if self.ulc.GetFocusedItem() > -1:
+            self.ulc.DeleteItem(self.ulc.GetFocusedItem())
 
     def openSupplierFiles(self, event):
         dlg = wx.FileDialog(
@@ -94,18 +128,38 @@ class MainForm(wx.Frame):
             defaultFile="", wildcard="*.*", style=wx.FD_MULTIPLE)
         if dlg.ShowModal() == wx.ID_OK:
             pathList = dlg.GetPaths()
-            myGrid = wx.GridSizer(len(pathList), 2, 0 ,0)
-            for item in pathList:
-                myGrid.Add((wx.TextCtrl(self.workPanel,wx.ID_ANY, item)), flag=wx.EXPAND, proportion=1)
-                myGrid.Add((wx.Choice(self.workPanel)), flag=wx.EXPAND, proportion=1)
-            self.workPanel.SetSizer(myGrid)
+            fileNames = dlg.GetFilenames()
+            for i in range(0,len(pathList)):
+                cm = wx.Choice(self.ulc, id=i)
+                cm.AppendItems(self.l)
+                self.ulc.InsertStringItem(i,pathList[i])
+                self.ulc.SetItemWindow(i, 1, cm, ULC.ULC_ALIGN_LEFT)
+        dlg.Destroy()
+
+
+    def add_Cheescake_Report(self, event):
+        dlg = wx.FileDialog(
+            self, message="Добавить файл-отчет выгруженный из Cheescake...",
+            defaultDir=".",
+            defaultFile="", wildcard="*.*", style=wx.FD_OPEN)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.input_CheesCake.LabelText = dlg.GetFilename()
+        dlg.Destroy()
+
+
+    def add_Comparision_Report(self, event):
+        dlg = wx.FileDialog(
+            self, message="Добавить таблицу соответствий...",
+            defaultDir=".",
+            defaultFile="", wildcard="*.*", style=wx.FD_OPEN)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.input_comparision.LabelText = dlg.GetFilename()
         dlg.Destroy()
 
 
     def onAbout(self, event):
-        dlg = wx.MessageDialog(self, "Message in MessageDialog","Title", wx.OK)
+        dlg = wx.MessageDialog(self, "Message in MessageDialog", "Title", wx.OK)
         dlg.ShowModal()
-
 
     def onExit(self, event):
         wx.Exit()
